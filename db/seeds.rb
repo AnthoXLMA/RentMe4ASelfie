1527ebec6a2930b6b9e469186db856a80eeabb22
AnthoXLMA <contact@service-lite-plus.com>
2021-12-28 21:27:15 +0100

    height&width Photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,6 @@ product1 = Product.new(
     name: 'Tony Parker',
     category: 'Sport',
     price_per_rent: 200
-    # photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 )
 product1.photo.attach(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 product1.save
@@ -17,7 +16,6 @@ product2 = Product.new(
     name: 'Lamborghini',
     category: 'Car',
     price_per_rent: 50
-    # photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
   )
 product2.photo.attach(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 product2.save
@@ -26,7 +24,6 @@ product3 = Product.new(
     name: 'Clown',
     category: 'Diary',
     price_per_rent: 30
-    # photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 )
 product3.photo.attach(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 product3.save
@@ -35,7 +32,6 @@ product4 = Product.new(
     name: 'Stomy Bugsy',
     category: 'Music',
     price_per_rent: 200
-    # photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 )
 product4.photo.attach(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 product4.save
@@ -44,7 +40,6 @@ product5 = Product.new(
     name: 'Vincent Cassel',
     category: 'Celebrity',
     price_per_rent: 400
-    # photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 )
 product5.photo.attach(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 product5.save
@@ -53,7 +48,6 @@ product6 = Product.new(
     name: 'Tony Parker',
     category: 'Sport',
     price_per_rent: 200
-    # photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 )
 product6.photo.attach(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 product6.save
@@ -62,7 +56,6 @@ product7 = Product.new(
     name: 'Ferrari F40',
     category: 'Vehicles',
     price_per_rent: 200
-    # photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 )
 product7.photo.attach(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 product7.save
@@ -71,7 +64,6 @@ product8 = Product.new(
     name: 'Ducati 40',
     category: 'Moto',
     price_per_rent: 150
-    # photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 )
 product8.photo.attach(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 product8.save
@@ -80,7 +72,6 @@ product9 = Product.new(
     name: 'Britney Spears',
     category: 'Celebrity',
     price_per_rent: 230
-    # photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 )
 product9.photo.attach(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 product9.save
@@ -89,73 +80,9 @@ product10 = Product.new(
     name: 'Yacht 450TDS',
     category: 'Boats',
     price_per_rent: 200
-    # photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 )
 product10.photo.attach(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
 product10.save
-# products = Product.create([
-#   {
-#     name: 'Tony Parker',
-#     category: 'Sport',
-#     price_per_rent: 200,
-#     photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
-#   },
-#   {
-#     name: 'Lamborghini',
-#     category: 'Car',
-#     price_per_rent: 50,
-#     photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
-#   },
-#   {
-#     name: 'Clown',
-#     category: 'Diary',
-#     price_per_rent: 30,
-#     photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
-#   },
-#   {
-#     name: 'Stomy Bugsy',
-#     category: 'Music',
-#     price_per_rent: 200,
-#     photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
-#   },
-#   {
-#     name: 'Vincent Cassel',
-#     category: 'Celebrity',
-#     price_per_rent: 400,
-#     photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
-#   },
-#   {
-#     name: 'Ferrari F40',
-#     category: 'Vehicles',
-#     price_per_rent: 200,
-#     photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
-#   },
-#   {
-#     name: 'Ducati 40',
-#     category: 'Moto',
-#     price_per_rent: 150,
-#     photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
-#   },
-#   {
-#     name: 'Britney Spears',
-#     category: 'Celebrity',
-#     price_per_rent: 230,
-#     photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
-#   },
-#   {
-#     name: 'Yacht 450TDS',
-#     category: 'Boats',
-#     price_per_rent: 200,
-#     photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
-#   },
-#   {
-#     name: 'Legos World',
-#     category: 'Games',
-#     price_per_rent: 400,
-#     photo: url(io: File.open(Rails.root.join('app/assets/images/lambo.jpeg')), filename: 'lambo.jpeg')
-#   }
-# ])
-# products.photo.attach(io: file, filename: 'lambo.jpeg', content_type: 'image/jpeg')
-# products.save
+
 # AdminUser.create!(email: 'admi@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 puts 'Finished!'
